a962c45572b531b1dff6070a038bfeb72ff67e18
mackscript <riponhaldar006@gmail.com>
2024-10-10 23:55:47 +0530

    first commit

--- android/app/src/main/java/com/mikutask/MainApplication.kt
+++ android/app/src/main/java/com/mikutask/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mikutask
+package com.rebinTask
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/mikuTaskTests/mikuTaskTests.m
+++ ios/mikuTaskTests/mikuTaskTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mikuTaskTests : XCTestCase
+@interface rebinTaskTests : XCTestCase
 
 @end
 
-@implementation mikuTaskTests
+@implementation rebinTaskTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
